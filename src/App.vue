69d23f870361e779ae0358ee517c8dc193306e9b
xiewenwen <1121099970@qq.com>
2021-10-31 14:57:00 +0800

    ✨ ref参数使用学习和外部参数props的使用学习

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-        <School></School>
+        <School ref="school"></School>
         <Student></Student>
+        <h2 v-text="msg" ref="title" id='title'></h2><button @click="show" ref="btn">show</button>
+        <!-- :age的意思是后面的是 按照表达式运算 -->
+        <StuProps name="stone" gender="men" :age="20+1"/>
   </div>
 </template>
 
@@ -11,13 +14,33 @@
 // import HelloWorld from './components/HelloWorld.vue'
 import School from './components/School'
 import Student from './components/Student'
+import StuProps from './components/StuProps'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
       School,
-      Student
+      Student,
+      StuProps
+      
+  },
+  data(){
+    return {
+      msg:'hello'
+    }
+  },
+  methods:{
+    show(){
+      //ref用于给元素或者组件打上标签，用于找到对应的标签信息
+      console.log(this.msg);
+      //传统获取dom的方式
+      console.log(document.getElementById('title'));
+      //vue下获取对应的dom元素
+      console.log(this.$refs.title);
+      //组件也可获取，获取组件，获取的是组件vc实例
+      console.log(this.$refs.school);      
+    }
   }
 }
 </script>
---
+++ src/components/StuProps.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+    <h2>{{ name }}</h2>
+    <h2>{{ gender }}</h2>
+    <h2>{{ age }}</h2>
+    <h2>{{myage}}</h2>
+    <button @click="updateage">updateage</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StuProps",
+  data() {
+    return {
+      //组件下能出现的
+      msg: "hello!welcome to here",
+      //外部传递的参数，age不能在方法内直接进行修改，需要把传递过来的数值赋值给data下面的参数
+      //通过改修data下的参数来触发
+      //外部传递的参数的优先级别大于内部参数
+      myage:this.age
+    }
+  },
+    methods:{
+        updateage(){
+            this.myage=99;
+        }
+    },
+
+  //接收参数，简单接收
+  // props:['name','gender','age']
+
+  ///高级接收，限定的参数的类型
+  //     props: {
+  //   title: String,
+  //   likes: Number,
+  //   isPublished: Boolean,
+  //   commentIds: Array,
+  //   author: Object,
+  //   callback: Function,
+  //   contactsPromise: Promise // or any other constructor
+  // }
+
+  //高级接收，接收的同时加上必要性和默认值指定限制和类型限制
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      default: 21,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
