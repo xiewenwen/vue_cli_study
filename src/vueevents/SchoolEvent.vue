2e6b433a5b8d7ff5442695d88edae4ff7f1ef057
xiewenwen <1121099970@qq.com>
2021-11-07 15:22:33 +0800

    ✨ 组件的自定义事件

---
+++ src/vueevents/SchoolEvent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="demo">
+        <h2>{{name}}</h2>
+        <h2>{{home}}</h2>
+    </div>
+</template>
+<script>
+    export default{
+        name:'School',
+        data(){
+        return{
+            name:'ashin',
+            home:'zg'
+        }        
+    }
+    }
+//备注
+</script>
+<style>
+ .demo{
+     background-color: brown;
+ }
+</style>
