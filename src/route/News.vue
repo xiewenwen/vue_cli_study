ee6c4c53bb098473eb437f91e4f422722f12492d
xiewenwen <1121099970@qq.com>
2021-11-22 20:54:16 +0800

    🤔 kepp-alive的使用

--- src/route/News.vue
+++ src/route/News.vue
@@ -13,7 +13,12 @@
         {{n.name}}
         </router-link>    
     </li>
+    <!-- kepp-alive 保存路由组件的缓存 不会被销毁 include下写的是组件名 指定组件不被销毁 
+        持续挂载，不被销毁 不然在切换路由的时候 组件就会被销毁 下面的数据都不会保存了
+    -->
+    <keep-alive include="News">
     <router-view></router-view>
+    </keep-alive>
 </ul>
   
 </template>
